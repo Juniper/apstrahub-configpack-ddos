13d07a19ecea45947dcff8850109190959660dea
Rajagopalan Subrahmanian <rajagopalans@gmail.com>
2025-07-31 07:52:20 -0400

    remove extra _

--- pack/tofu/service_registry.tf
+++ pack/tofu/service_registry.tf
@@ -1,6 +1,6 @@
 resource "apstra_raw_json" "ddos_service" {
   url = "/api/telemetry-service-registry"
-  id = "DDoS_Protection_Protocols__${var.blueprint_id}"
+  id = "DDoS_Protection_Protocols_${var.blueprint_id}"
   payload = jsonencode(
     {
       description        = "DDOS service registry"
