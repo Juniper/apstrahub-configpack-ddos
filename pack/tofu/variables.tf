db2a02ecbaee583e2790a6108e94a1991f0bea44
rajagopalans <rajagopalans@gmail.com>
2025-08-14 13:41:28 -0400

    update regex to use -

--- pack/tofu/variables.tf
+++ pack/tofu/variables.tf
@@ -4,10 +4,11 @@ variable "blueprint_id" {
 
 variable "name" {
   type = string
-  description = "Name: Please use only letters, numbers and _"
+
+  description = "Name: Please use only letters, numbers, - and _"
   validation {
-    condition     = !can(regex("[^A-Za-z0-9_]", var.name))
-    error_message = "The value must not contain any characters other than letters, numbers, and underscores (_)."
+    condition     = !can(regex("[^A-Za-z0-9_-]", var.name))
+    error_message = "The name must only have letters, numbers, - and _"
   }
   default = "DDOS"
 }
